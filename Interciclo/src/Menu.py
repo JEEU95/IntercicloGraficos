989262a9046409f310114c2da441bae04ebe7d79
JEEU95 <jeeu.95@gmail.com>
2020-05-27 20:14:40 -0500

    Menu por consola

---
+++ Interciclo/src/Menu.py
@@ -0,0 +1,9 @@
+import Cuadrado
+if __name__ == "__main__":
+    print("Reconocimiento de figuras")
+    print("1. Reconocimiento de Circulos")
+    print("2. Reconocimiento de Cuadrados")
+    Cuadrado.print_func("Prueba")
+    print("3. Reconocimiento de Rectangulos")
+    print("4. Reconocimiento de Triangulos")
+    print("Salir")
--- Interciclo/src/interciclo.py
+++
@@ -1,2 +0,0 @@
-if __name__ == "__main__":
-    print("Prueba 1")
