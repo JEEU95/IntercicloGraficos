3fb97416dae89ff2f2adf9c4fc006cdfbbcab595
JEEU95 <jeeu.95@gmail.com>
2020-06-01 00:56:50 -0500

    a

--- Interciclo/src/Menu.py
+++ Interciclo/src/Menu.py
@@ -1,9 +1,15 @@
 import Cuadrado
-if __name__ == "__main__":
+def menu():
     print("Reconocimiento de figuras")
     print("1. Reconocimiento de Circulos")
     print("2. Reconocimiento de Cuadrados")
-    Cuadrado.print_func("Prueba")
     print("3. Reconocimiento de Rectangulos")
     print("4. Reconocimiento de Triangulos")
-    print("Salir")
+    print("Esc. Salir")
+
+if __name__ == "__main__":
+    Ejec=1
+    while(Ejec == 1):
+        menu()
+        Ejec = 0
+        
